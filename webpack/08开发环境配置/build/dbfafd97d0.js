7c7eac24ffc6b336b52166d5adcc577198a8d8b6
Spike Spiegel <theumweltsky@gmail.com>
2021-08-26 07:29:04 +0800

    feat:开发环境打包设置

---
+++ webpack/08开发环境配置/build/dbfafd97d0.js
@@ -0,0 +1,9 @@
+import "./index.less"
+// 引入 iconfont 样式文件
+
+import "./iconfont.css"
+
+function add(x, y) {
+    return x + y
+}
+console.log("add", add(2, 4))
---
+++ webpack/08开发环境配置/src/js/index.js
@@ -0,0 +1,8 @@
+import "../css/index.less"
+// 引入 iconfont 样式文件
+import "../css/iconfont.css"
+
+function add(x, y) {
+    return x + y
+}
+console.log("add", add(2, 4))
